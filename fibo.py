a02b119364ca9a08b1f4b1e66994d305262b4879
Jakub Kral <jakub.g.kral@gmail.com>
2025-04-28 13:33:52 +0200

    fibo

--- fibo.py
+++ fibo.py
@@ -1,5 +1,16 @@
+def recursive_ntb_fibo(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return recursive_ntb_fibo(n-1)+recursive_ntb_fibo(n-2)
+
 
 def main():
+    n=int(input("Počet členu fibonacciho posloupnosti:"))
+    seq=[recursive_ntb_fibo(num) for num in range(n+1)]
+    print(seq)
     pass
 
 
